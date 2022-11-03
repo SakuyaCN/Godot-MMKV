1f4177d49a1d0db99eac51e8aa60f4915f60f9d7
咲夜 <347764670@qq.com>
2022-11-03 21:59:30 +0800

    123

--- Test.gd
+++ Test.gd
@@ -1,16 +1,42 @@
 extends Control
 
-#提供一个可访问
 var mmkv = load("res://addons/godot-mmkv/mmkv.gd").new()
 
 func _ready():
 	mmkv.initMMKV()
 	loadData()
-
+	
+	mmkvWrite()
+	mmkvRead()
+	
+	fileWrite()
+	fileRead()
+	
 func loadData():
 	$Control2/HP.text = "HP:%s"%str(PlayerData.hp)
-	$Control2/NAME.text ="昵称：" +PlayerData.player_name
+	$Control2/NAME.text ="昵称：" + PlayerData.player_name
 	$Control2/dict.text = to_json(PlayerData.attr)
+	$Control2/Lv.text = "LV:%s" %str(PlayerData.lv)
+
+func mmkvWrite():
+	var a = Time.get_ticks_usec()
+	mmkv.setString("asd",str(randf()))
+	$Control/Label3.text += "%s /us"%str(Time.get_ticks_usec() - a)
+
+func mmkvRead():
+	var a = Time.get_ticks_usec()
+	mmkv.getString("asd")
+	$Control/Label5.text += "%s /us"%str(Time.get_ticks_usec() - a)
+
+func fileWrite():
+	var a = Time.get_ticks_usec()
+	saveFile(str(randf()))
+	$Control/Label4.text += "%s /us"%str(Time.get_ticks_usec() - a)
+
+func fileRead():
+	var a = Time.get_ticks_usec()
+	loadFile()
+	$Control/Label6.text += "%s /us"%str(Time.get_ticks_usec() - a)
 
 func mmkvTest():
 	var a = Time.get_ticks_msec()
@@ -45,10 +71,19 @@ func _on_Button_pressed():
 	fileTest()
 
 func _on_Button2_pressed():
-	PlayerData.player_name = "昵称：咲夜" + str(randf())
+	PlayerData.player_name = "咲夜" + str(randf())
 	PlayerData.hp = randi()% 100
 	PlayerData.attr = {
 		"exp":randi()%100,
 		"atk":randi()%500
 	}
+	PlayerData.lv += 1
 	loadData()
+
+
+func _on_Timer_timeout():
+	PlayerData.lv += 1
+	loadData()
+
+func _on_Button3_pressed():
+	$Timer.stop()
--- autoLoad/PlayerData.gd
+++ autoLoad/PlayerData.gd
@@ -2,11 +2,12 @@ extends NodeMMKV
 
 var player_name = "sakuya"
 var hp = 0
+var lv = 1
 var attr = {}
 
 func _init():
-	isBinding = true
-	save_tikc = 0.05
+	isBinding = true #开启自动绑定
+	save_tikc = 0.05 #自动存储间隔 ms
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
